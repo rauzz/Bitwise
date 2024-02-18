c8e0b24f10a47d3ebb8fc92f631baf3b3ce2b3cf
Marcin Izdebski <marcinok24@gmail.com>
2024-02-18 16:51:02 +0100

    add burger menu for mobile devices

---
+++ js/burgerNav.js
@@ -0,0 +1,19 @@
+const burger = document.querySelector(".burger");
+const burgerIcon = document.querySelector(".burgerIcon");
+const xIcon = document.querySelector(".xIcon");
+const navAside = document.querySelector(".nav-aside");
+const buttons = document.querySelectorAll("a");
+
+burger.addEventListener("click", function () {
+	burgerIcon.classList.toggle("show");
+	xIcon.classList.toggle("show");
+	navAside.classList.toggle("active");
+});
+
+buttons.forEach((button) => {
+	button.addEventListener("click", function () {
+		burgerIcon.classList.toggle("show");
+		xIcon.classList.toggle("show");
+		navAside.classList.toggle("active");
+	});
+});
